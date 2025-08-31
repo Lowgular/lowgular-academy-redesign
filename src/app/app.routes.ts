b63efd326529ca1d8c8a9e8fd736bd39d2664369
Greg <grzegorz@lowgular.io>
2025-08-31 08:55:59 +0100

    added design system

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CourseDetailPage } from './pages/course-detail.page';
+import { DesignSystemPage } from './pages/design-system.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: DesignSystemPage,
+  },
+  {
+    path: 'course-detail',
+    component: CourseDetailPage,
+  },
+];
---
+++ src/app/pages/course-detail.page.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './course-detail.page.html',
+})
+export class CourseDetailPage {}
